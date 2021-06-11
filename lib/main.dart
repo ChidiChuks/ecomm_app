58370f91785e813333ff92e928735a398d2c5da6
Wrokbrook repo <chukschiboy@gmail.com>
2021-06-11 09:56:45 -0700

    update of the value changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:ecomm_app/pages/login.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.red.shade900,
+    ),
     home: Login(),
   ));
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,111 +1,28 @@
-import 'package:ecomm_app/pages/signup.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 //Personal Imports
 import 'package:ecomm_app/pages/home.dart';
 
-class Login extends StatefulWidget {
+class SignUp extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _SignUpState createState() => _SignUpState();
 }
 
-class _LoginState extends State<Login> {
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
+class _SignUpState extends State<SignUp> {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
 
   TextEditingController _emailTextController = TextEditingController();
   TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _nameTextController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+  String gender;
+  String groupValue = "male";
 
-  SharedPreferences preferences;
+  // SharedPreferences preferences;
   bool loading = false;
   bool isLoggedIn = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isSignedIn();
-  }
-
-  void isSignedIn() async {
-    setState(() {
-      loading = true;
-    });
-
-    preferences = await SharedPreferences.getInstance();
-    isLoggedIn = await googleSignIn.isSignedIn();
-
-    if (isLoggedIn) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => new HomePage()),
-      );
-    }
-
-    setState(() {
-      loading = false;
-    });
-  }
-
-  Future handleSignIn() async {
-    preferences = await SharedPreferences.getInstance();
-
-    setState(() {
-      loading = true;
-    });
-
-    GoogleSignInAccount googleUser = await googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication =
-        await googleUser.authentication;
-    FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken);
-
-    if (firebaseUser != null) {
-      final QuerySnapshot result = await Firestore.instance
-          .collection("users")
-          .where("id", isEqualTo: firebaseUser.uid)
-          .getDocuments();
-
-      final List<DocumentSnapshot> documents = result.documents;
-
-      if (documents.length == 0) {
-        // Insert the user into the collection
-        Firestore.instance
-            .collection("users")
-            .document(firebaseUser.uid)
-            .setData({
-          "id": firebaseUser.uid,
-          "username": firebaseUser.displayName,
-          "profilePicture": firebaseUser.photoUrl,
-        });
-
-        await preferences.setString("id", firebaseUser.uid);
-        await preferences.setString("username", firebaseUser.displayName);
-        await preferences.setString("photoUrl", firebaseUser.displayName);
-      } else {
-        await preferences.setString("id", documents[0]['id']);
-        await preferences.setString("username", documents[0]['username']);
-        await preferences.setString("photoUrl", documents[0]["photoUrl"]);
-      }
-
-      Fluttertoast.showToast(msg: "Successfully logged in");
-      setState(() {
-        loading = false;
-      });
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()),
-      );
-    } else {
-      Fluttertoast.showToast(msg: "Login failed");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -156,7 +73,7 @@ class _LoginState extends State<Login> {
           ),
           // Login Form
           Padding(
-            padding: const EdgeInsets.only(top: 270.0),
+            padding: const EdgeInsets.only(top: 170.0),
             child: Container(
               alignment: Alignment.center,
               child: Center(
@@ -164,6 +81,77 @@ class _LoginState extends State<Login> {
                   key: _formKey,
                   child: ListView(
                     children: [
+                      // Name
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.8),
+                          elevation: 0.0,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 12.0),
+                            child: TextFormField(
+                              controller: _nameTextController,
+                              decoration: InputDecoration(
+                                hintText: "Full name",
+                                icon: Icon(Icons.person_outline),
+                                // border: OutlineInputBorder(),
+                              ),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "The name field cannot be empty";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // Sex value
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                        child: Container(
+                          color: Colors.white.withOpacity(0.45),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(
+                                    "male",
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  trailing: Radio(
+                                    // fillColor: ,
+                                    activeColor: Theme.of(context).primaryColor,
+                                    value: "male",
+                                    groupValue: groupValue,
+                                    onChanged: (e) => valueChanged(e),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(
+                                    "female",
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  trailing: Radio(
+                                    value: "female",
+                                    groupValue: groupValue,
+                                    onChanged: (e) => valueChanged(e),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
                       // Email Input Tab
                       Padding(
                         padding:
@@ -226,6 +214,35 @@ class _LoginState extends State<Login> {
                           ),
                         ),
                       ),
+                      // Confirm password
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white.withOpacity(0.8),
+                          elevation: 0.0,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 12.0),
+                            child: TextFormField(
+                              controller: _confirmPasswordController,
+                              decoration: InputDecoration(
+                                hintText: "Confirm Password ",
+                                icon: Icon(Icons.lock_outline),
+                                // border: OutlineInputBorder(),
+                              ),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "The password field cannot be empty";
+                                } else if (value.length < 6) {
+                                  return "the password has to be at least 6 characters long";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
                       // Login Button
                       Padding(
                         padding:
@@ -238,7 +255,7 @@ class _LoginState extends State<Login> {
                             onPressed: () {},
                             minWidth: MediaQuery.of(context).size.width,
                             child: Text(
-                              "Login",
+                              "Sign up",
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                 color: Colors.white,
@@ -249,56 +266,16 @@ class _LoginState extends State<Login> {
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Container(
-                          padding:
-                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
-                          child: Text(
-                            "Forgot password",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 13.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
+
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Center(
-                          child:
-                              // RichText(
-                              //   text: TextSpan(
-                              //     style: TextStyle(
-                              //       color: Colors.white,
-                              //       fontWeight: FontWeight.w400,
-                              //       fontSize: 16.0,
-                              //     ),
-                              //     children: [
-                              //       TextSpan(
-                              //         text: "Don't have an account? click here to",
-                              //       ),
-                              //       TextSpan(
-                              //         text: " sign up!",
-                              //         style: TextStyle(
-                              //           color: Colors.red,
-                              //         ),
-                              //       ),
-                              //     ],
-                              //   ),
-                              // ),
-                              InkWell(
+                          child: InkWell(
                             onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SignUp(),
-                                ),
-                              );
+                              Navigator.pop(context);
                             },
                             child: Text(
-                              "Sign up",
+                              "Login",
                               textAlign: TextAlign.center,
                               style: TextStyle(color: Colors.red),
                             ),
@@ -310,7 +287,7 @@ class _LoginState extends State<Login> {
                         color: Colors.white,
                       ),
                       Text(
-                        "Other login option",
+                        "Other sign up option",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.white,
@@ -328,7 +305,7 @@ class _LoginState extends State<Login> {
                           elevation: 0.0,
                           child: MaterialButton(
                             onPressed: () {
-                              handleSignIn();
+                              // handleSignIn();
                             },
                             minWidth: MediaQuery.of(context).size.width,
                             child: Text(
@@ -380,18 +357,16 @@ class _LoginState extends State<Login> {
       // ),
     );
   }
-}
 
-// keyboardType: TextInputType.emailAddress,
-//                       controller: _emailTextController,
-//                       validator: (value) {
-//   if (value.isEmpty) {
-//     Pattern pattern =
-//         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-//     RegExp regex = new RegExp(pattern);
-//     if (!regex.hasMatch(value))
-//       return 'Please make sure your email address is valid';
-//     else
-//       return null;
-//   }
-// },
+  valueChanged(e) {
+    setState(() {
+      if (e == "male") {
+        groupValue = e;
+        gender =  e;
+      } else if (e == "female") {
+        groupValue = e;
+        gender = e;
+      }
+    });
+  }
+}
